7b864509e1fef6fc7b91026782dda34025a070c1
RazorSh4rk <iamrazorshark@gmail.com>
2026-02-07 22:16:33 -0800

    change port for gcp

--- app.py
+++ app.py
@@ -119,4 +119,5 @@ def serve_pdf(filename):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
